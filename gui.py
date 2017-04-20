85763e61fb2f1ec2447d2262dbb4696aad3165ef
Colin Williams <Colin Williams>
2017-04-20 16:53:23 +0200

    implemented a working framework for the gui; cleaned up reading and writing to papers.rdf file

--- gui.py
+++ gui.py
@@ -5,152 +5,161 @@ This is my first use of PyQt
 
 I heavily consulted the following tutorial:
 http://zetcode.com/gui/pyqt5/
+as well as this post on dialogs:
+http://stackoverflow.com/questions/18196799/how-can-i-show-a-pyqt-modal-dialog-and-get-data-out-of-its-controls-once-its-clo
 
 author: Colin Williams
-last edited: Jan. 2016
+last edited: April. 2017
 """
 
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget,
     QPushButton, QDesktopWidget, QLabel, QLineEdit,
-    QTextEdit, QGridLayout, QDateEdit, QSpinBox, )
-
-#from PyQt5.QtGui import QIcon, QHBoxLayout, QVBoxLayout
+    QTextEdit, QGridLayout, QDateEdit, QSpinBox, QDialog, QDialogButtonBox)
 from PyQt5.QtCore import QCoreApplication
 
-# TODO: link menus together
+from collections import deque
+
 # TODO: link to papers.py
 # TODO: test on linux
 
-class numAuthors(QWidget):
-
+###############################################################
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-
         self.initUI()
-
     def initUI(self):
-        prompt = QLabel("How many authors?")
-        num = QSpinBox(self)
 
-        self.dialog = Authors()
+        # BUTTONS
+        addAuthorButton = QPushButton('Add Author', self)
+        addPaperButton = QPushButton('Add Paper', self)
+        submitButton = QPushButton('Submit Changes', self)
+        addAuthorButton.clicked.connect(self.add_author)
+        addPaperButton.clicked.connect(self.add_paper)
+        submitButton.clicked.connect(self.submit_changes)
 
-        # Buttons
-        submitButton = QPushButton('Submit', self)
-        quitButton = QPushButton('Quit', self)
+        # RePEc ENTRY
+        self.entryEdit = QTextEdit()
 
-        # Listeners
-        quitButton.clicked.connect(QCoreApplication.instance().quit)
-        submitButton.clicked.connect(self.on_submitButton_clicked)
-
-        # Layout
+        # LAYOUT
         grid = QGridLayout()
         grid.setSpacing(10)
-
-        grid.addWidget(prompt, 1, 0)
-        grid.addWidget(num, 1, 1)
-
-        grid.addWidget(submitButton, 2, 0)
-        grid.addWidget(quitButton, 2, 1)
-
+        grid.addWidget(addAuthorButton, 0, 0)
+        grid.addWidget(addPaperButton, 0, 1)
+        grid.addWidget(submitButton, 0, 2)
+        grid.addWidget(self.entryEdit, 1, 0, 1, 3)
         self.setLayout(grid)
 
-        self.resize(280, 150)
-        self.center()
-        self.setWindowTitle('Number of Authors')
+        self.resize(400, 400)
+        self.setWindowTitle("Update RePEc File")
 
         self.show()
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def add_author(self):
+        auth, ok = Author.getAuthorInfo()
+        if ok:
+            for line in auth:
+                self.entryEdit.append(line)
 
-    def on_submitButton_clicked(self):
-        self.dialog.show()
+    def add_paper(self):
+        paper, ok = Paper.getPaperInfo()
 
-class Authors(QWidget):
+    def submit_changes(self):
+        pass
 
-    def __init__(self):
-        super().__init__()
+###############################################################
+class Author(QDialog):
 
+    def __init__(self, parent = None):
+        super(Author, self).__init__(parent)
         self.initUI()
-
     def initUI(self):
-
         # Labels
         fName = QLabel("First Name: ")
         mName = QLabel("Middle Initial: ")
         lName = QLabel("Last Name: ")
-        # TODO: RePEc code
+        authorCode = QLabel("RePEc code: ")
         workplace = QLabel("Workplace: ")
         homepage = QLabel("Homepage: ")
 
         # Edits
-        fNameEdit = QLineEdit()
-        mNameEdit = QLineEdit()
-        lNameEdit = QLineEdit()
-        workplaceEdit = QLineEdit()
-        homepageEdit = QLineEdit()
+        self.fNameEdit = QLineEdit()
+        self.mNameEdit = QLineEdit()
+        self.lNameEdit = QLineEdit()
+        self.codeEdit = QLineEdit()
+        self.workplaceEdit = QLineEdit()
+        self.homepageEdit = QLineEdit()
 
         # Buttons
+        buttons = QDialogButtonBox(
+            QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        buttons.accepted.connect(self.accept)
+        buttons.rejected.connect(self.reject)
         williamsButton = QPushButton('Williams Author', self)
-        submitButton = QPushButton('Submit', self)
-        quitButton = QPushButton('Quit', self)
-
 
         # Listeners
-        quitButton.clicked.connect(QCoreApplication.instance().quit)
+        williamsButton.clicked.connect(self.on_WilliamsButton_clicked)
 
         # Layout
         grid = QGridLayout()
         grid.setSpacing(10)
 
         grid.addWidget(lName, 1, 0)
-        grid.addWidget(lNameEdit, 1, 1)
+        grid.addWidget(self.lNameEdit, 1, 1)
         grid.addWidget(williamsButton, 1, 2)
-
         grid.addWidget(fName, 2, 0)
-        grid.addWidget(fNameEdit, 2, 1)
-
+        grid.addWidget(self.fNameEdit, 2, 1)
         grid.addWidget(mName, 3, 0)
-        grid.addWidget(mNameEdit, 3, 1)
-
-        grid.addWidget(workplace, 4, 0)
-        grid.addWidget(workplaceEdit, 4, 1)
-
-        grid.addWidget(homepage, 5, 0)
-        grid.addWidget(homepageEdit, 5, 1)
-
-        grid.addWidget(submitButton, 6, 0)
-        grid.addWidget(quitButton, 6, 1)
-
+        grid.addWidget(self.mNameEdit, 3, 1)
+        grid.addWidget(authorCode, 4, 0)
+        grid.addWidget(self.codeEdit, 4, 1)
+        grid.addWidget(workplace, 5, 0)
+        grid.addWidget(self.workplaceEdit, 5, 1)
+        grid.addWidget(homepage, 6, 0)
+        grid.addWidget(self.homepageEdit, 6, 1)
+        grid.addWidget(buttons, 7, 0)
         self.setLayout(grid)
 
         # Focus and tab order
-        lNameEdit.setFocus(True)
-
-
+        self.lNameEdit.setFocus(True)
         self.resize(400, 400)
-        self.center()
-        self.setWindowTitle('Author') #TODO: add author number
+        self.setWindowTitle('Author')
 
-        #self.show()
+    def on_WilliamsButton_clicked(self):
+        pass
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def authorInfo(self):
+        info = deque()
 
-class Paper(QWidget):
+        # Build up the full name
+        fullName = self.fNameEdit.text() + " "
+        if self.mNameEdit.text():
+            fullName += (self.mNameEdit.text() + ". ")
+        fullName += self.lNameEdit.text()
 
-    def __init__(self):
-        super().__init__()
+        info.append('Author-Name: ' + fullName)
+        info.append('Author-X-Name-First: ' + self.fNameEdit.text())
+        info.append('Author-X-Name-Last: ' + self.lNameEdit.text())
+        info.append('Author-Person: ' + self.codeEdit.text())
+        info.append('Author-Workplace-Name: ' + self.workplaceEdit.text())
+        info.append('Author-Workplace-Homepage: ' + self.homepageEdit.text())
+        #info.append() #AUTHOR LAST NAME TO BUILD FILE URL
+        return info
+
+    @staticmethod
+    def getAuthorInfo(parent = None):
+        dialog = Author(parent)
+        result = dialog.exec_()
+        info = dialog.authorInfo()
+        return (info, result == QDialog.Accepted)
+
+###############################################################
+class Paper(QDialog):
+
+    def __init__(self, parent=None):
+        super(Paper, self).__init__(parent)
 
         self.initUI()
-
     def initUI(self):
         # Labels
         title = QLabel('Paper Title:')
@@ -167,13 +176,10 @@ class Paper(QWidget):
         titleEdit = QLineEdit()
         classEdit = QLineEdit()
         keywordsEdit = QLineEdit()
-
         lengthEdit = QSpinBox(self)
         numberEdit = QSpinBox(self)
-
         createdEdit = QDateEdit()
         urlTitleEdit = QLineEdit("http://web.williams.edu/Economics/wp/")
-
         abstractEdit = QTextEdit()
 
         # Buttons
@@ -186,35 +192,25 @@ class Paper(QWidget):
         # Layout
         grid = QGridLayout()
         grid.setSpacing(10)
-
         grid.addWidget(title, 1, 0)
         grid.addWidget(titleEdit, 1, 1, 1, 2)
-
         grid.addWidget(classification, 2, 0)
         grid.addWidget(classEdit, 2, 1, 1, 2)
-
         grid.addWidget(keywords, 3, 0)
         grid.addWidget(keywordsEdit, 3, 1, 1, 2)
-
         grid.addWidget(length, 4, 0)
         grid.addWidget(lengthEdit, 4, 1, 1, 2)
         grid.addWidget(pages, 4, 3)
-
         grid.addWidget(number, 5, 0)
         grid.addWidget(numberEdit, 5, 1, 1, 2)
-
         grid.addWidget(created, 6, 0)
         grid.addWidget(createdEdit, 6, 1, 1, 2)
-
         grid.addWidget(urlTitle, 7, 0)
         grid.addWidget(urlTitleEdit, 7, 1, 1, 2)
-
         grid.addWidget(abstract, 8, 0)
         grid.addWidget(abstractEdit, 8, 1, 1, 2)
-
         grid.addWidget(submitButton, 9, 1)
         grid.addWidget(quitButton, 9, 2)
-
         self.setLayout(grid)
 
         # Focus and tab order
@@ -228,7 +224,18 @@ class Paper(QWidget):
         self.center()
         self.setWindowTitle('Paper')
 
-        self.show()
+    def paperInfo(self):
+        info = []
+        return info
+
+    @staticmethod
+    def getPaperInfo(parent = None):
+        dialog = Paper(parent)
+        result = dialog.exec_()
+        info = dialog.paperInfo()
+        return (info, result == QDialog.Accepted)
+
+        #self.show()
 
     def center(self):
         qr = self.frameGeometry()
@@ -236,11 +243,10 @@ class Paper(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+###############################################################
 def main():
     app = QApplication(sys.argv)
-    #paper = Paper()
-    num = numAuthors()
-    #auth = Authors()
+    win = MainWindow()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
--- papers.py
+++ papers.py
@@ -3,7 +3,7 @@ This is code to update the papers.rdf file that contains information
 about the working paper series at Williams College.
 
 Author: Colin Williams
-Last updated: Feb 2017
+Last updated: April 2017
 """
 
 import os
@@ -12,112 +12,153 @@ import pickle
 # TODO: Download .rdf file from server instead of storing on local machine
 # TODO: Upload .rdf file automatically (and safely!)
 # TODO: Generate html
+# TODO: make the user enter the year
 
+# The 'test' argument specifies whether the output will be written to
+# the actual .rdf file or whether it will be to a test file. For now,
+# the default is to a test file.
+def addNewEntry(entry = "", curYear = 2017, test = "-test"):
+    # Read in existing papers.pdf
+    data = ''
+    with open(cwd + 'RePEc' + os.sep + 'papers.rdf', 'r') as original:
+        line1 = original.readline() # Ignore the first line of #s
+        line2 = original.readline()
+        if curYear in line2: # Check whether to update the year header
+            line3 = original.readline() # Skip the last row of #s
+            data = original.read()
+            print("Year unchanged: " + curYear)
+        else:
+            data = line1 + line2 + original.read()
+            print('It\'s a new year: ' + curYear)
+    original.close()
+
+    # Write the new entry to the file
+    with open(cwd + 'RePEc' + os.sep + 'papers' + test + '.rdf', 'w') as modified:
+
+        ###### OUTPUTS FOR NEW ENTRY #####
+        modified.write('##################################################\n')
+        modified.write("#                     "+curYear+"                       #\n")
+        modified.write('##################################################\n')
+        modified.write('Template-Type: ReDIF-Paper 1.0\n')
+
+        modified.write(entry)
+        modified.write(data)
+
+    modified.close()
+
+###############################################################################
+# Simple code to add an entry through the command line. Works
+# on Windows and Linux, but is unable to read the abstract due to its length.
+###############################################################################
 # Make compatible with python2
 try:
     input = raw_input
 except NameError:
     pass
+    
+def simpleAddEntry():
+    curYear = '2017' # Update to keep header correct
+    cwd = os.getcwd() + os.sep
 
-curYear = '2017' # Update to keep header correct
-cwd = os.getcwd() + os.sep
+    RePEc_codes = pickle.load( open(cwd + "Faculty_Data" + os.sep + "RePEc_codes.p", "rb") )
+    Author_Homepage = pickle.load( open(cwd + "Faculty_Data" + os.sep + "Author_Homepage.p", "rb") )
 
-RePEc_codes = pickle.load( open(cwd + "Faculty_Data" + os.sep + "RePEc_codes.p", "rb") )
-Author_Homepage = pickle.load( open(cwd + "Faculty_Data" + os.sep + "Author_Homepage.p", "rb") )
-
-# Read in existing papers.pdf
-data = ''
-with open(cwd + 'RePEc' + os.sep + 'papers.rdf', 'r') as original:
-    line1 = original.readline() # Ignore the first line
-    line2 = original.readline()
-    if curYear in line2: # Check whether to update the year header
-        line3 = original.readline() # Skip the last row of #s
-        data = original.read()
-        print("Year unchanged: " + curYear)
-    else:
-        data = line1 + line2 + original.read()
-        print('It\'s a new year: ' + curYear)
-original.close()
-
-#Rename the old file as a backup
-#os.remove(cwd + "RePEc" + os.sep + "papers-old.rdf")
-#os.rename(cwd + 'RePEc' + os.sep + 'papers.rdf', cwd + 'RePEc' + os.sep + 'papers-old.rdf')
-
-###### INPUTS FOR NEW ENTRY ######
-lastNames = ''
-firstName, middleI, lastName, fullNameList = [], [], [], []
-numAuthors = int(input("How many authors?"))
-for i in range (0, numAuthors):
-    firstName.append(input("Author first name: "))
-    middleI.append(input("Author middle initial (no period, enter if none): "))
-    lastName.append(input("Author last name: "))
-
-    # Add together author last names in anticipation of the file url
-    lastNames += lastName[i]
-
-    # Build up the full name
-    fullName = firstName[i] + " "
-    if middleI[i]: #not sure this works if no middle name
-        fullName += (middleI[i] + ". " + lastName[i])
-    else:
-        fullName += lastName[i]
-    fullNameList.append(fullName)
-
-title = input("Title: ")
-# TODO: fix
-#abstract = input("Abstract: ")
-clasJEL = input("JEL-classification (separated by commas and spaces): ")
-keywords = input("Keywords (same format): ")
-length = input("Integer length: ")
-# TODO: make automatic
-number = input("Number (no year): ")
-created = input("Creation date (yyyy-mm): ")
-urlTitle = input("URL title: ")
-
-# TODO: make into function
-# Construct the new entry
-with open(cwd + 'RePEc' + os.sep + 'papers-test.rdf', 'w') as modified:
-
-    ###### OUTPUTS FOR NEW ENTRY #####
-    modified.write('##################################################\n')
-    modified.write("#                     "+curYear+"                       #\n")
-    modified.write('##################################################\n')
-    modified.write('Template-Type: ReDIF-Paper 1.0\n')
-
-    for i in range(0, numAuthors):
-        modified.write('Author-Name: ' + fullNameList[i] + '\n')
-        modified.write('Author-X-Name-First: ' + firstName[i] + '\n')
-        modified.write('Author-X-Name-Last: ' + lastName[i] + '\n')
-
-        modified.write('Author-Person: ')
-        if lastName[i] in RePEc_codes:
-            modified.write(RePEc_codes[lastName[i]])
-        modified.write('\n')
-
-        # Williams Faculty
-        if lastName[i] in Author_Homepage: # Problems when outside authors have the same name as Williams faculty
-            modified.write('Author-Workplace-Name: Williams College' + '\n')
-            modified.write('Author-Workplace-Homepage: https://econ.williams.edu/profile/' + Author_Homepage[lastName[i]] + '\n')
-        # Non-Williams Faculty
+    # Read in existing papers.pdf
+    data = ''
+    with open(cwd + 'RePEc' + os.sep + 'papers.rdf', 'r') as original:
+        line1 = original.readline() # Ignore the first line
+        line2 = original.readline()
+        if curYear in line2: # Check whether to update the year header
+            line3 = original.readline() # Skip the last row of #s
+            data = original.read()
+            print("Year unchanged: " + curYear)
         else:
-            modified.write('Author-Workplace-Name: ' + '\n')
-            modified.write('Author-Workplace-Homepage: ' + '\n')
+            data = line1 + line2 + original.read()
+            print('It\'s a new year: ' + curYear)
+    original.close()
 
-    modified.write("Title: " + title + '\n')
-    modified.write("Abstract: " + '\n')
-    modified.write("Classification-JEL: " + clasJEL + '\n')
-    modified.write("Keywords: " + keywords + '\n')
-    modified.write('Length: ' + length + ' pages' + '\n')
-    modified.write("Number: " + curYear + "-" + number + '\n')
-    modified.write("Note: " + '\n')
-    modified.write('Creation-date: ' + created + '\n')
-    modified.write('Revision-date:' + '\n' + 'Price: Free' + '\n' + 'Publication-Status:' + '\n')
-    modified.write('File-URL: http://web.williams.edu/Economics/wp/'+ lastNames + urlTitle + '.pdf\n')
+    #Rename the old file as a backup
+    #os.remove(cwd + "RePEc" + os.sep + "papers-old.rdf")
+    #os.rename(cwd + 'RePEc' + os.sep + 'papers.rdf', cwd + 'RePEc' + os.sep + 'papers-old.rdf')
 
-    modified.write('File-Format: Application/PDF' + '\n')
-    modified.write('File-Function: Full text' + '\n')
-    modified.write('Handle: RePEc:wil:wileco:' + curYear + '-' + number + '\n\n')
+    ###### INPUTS FOR NEW ENTRY ######
+    lastNames = ''
+    firstName, middleI, lastName, fullNameList = [], [], [], []
+    numAuthors = int(input("How many authors?"))
+    for i in range (0, numAuthors):
+        firstName.append(input("Author first name: "))
+        middleI.append(input("Author middle initial (no period, enter if none): "))
+        lastName.append(input("Author last name: "))
 
-    modified.write(data)
+        # Add together author last names in anticipation of the file url
+        lastNames += lastName[i]
 
-modified.close()
+        # Build up the full name
+        fullName = firstName[i] + " "
+        if middleI[i]:
+            fullName += (middleI[i] + ". " + lastName[i])
+        else:
+            fullName += lastName[i]
+        fullNameList.append(fullName)
+
+    title = input("Title: ")
+    clasJEL = input("JEL-classification (separated by commas and spaces): ")
+    keywords = input("Keywords (same format): ")
+    length = input("Integer length: ")
+    # TODO: make automatic
+    number = input("Number (no year): ")
+    created = input("Creation date (yyyy-mm): ")
+    urlTitle = input("URL title: ")
+
+    # Construct the new entry
+    with open(cwd + 'RePEc' + os.sep + 'papers-test.rdf', 'w') as modified:
+
+        ###### OUTPUTS FOR NEW ENTRY #####
+        modified.write('##################################################\n')
+        modified.write("#                     "+curYear+"                       #\n")
+        modified.write('##################################################\n')
+        modified.write('Template-Type: ReDIF-Paper 1.0\n')
+
+        for i in range(0, numAuthors):
+            modified.write('Author-Name: ' + fullNameList[i] + '\n')
+            modified.write('Author-X-Name-First: ' + firstName[i] + '\n')
+            modified.write('Author-X-Name-Last: ' + lastName[i] + '\n')
+
+            modified.write('Author-Person: ')
+            if lastName[i] in RePEc_codes:
+                modified.write(RePEc_codes[lastName[i]])
+            modified.write('\n')
+
+            # Williams Faculty
+            if lastName[i] in Author_Homepage: # Problems when outside authors have the same name as Williams faculty
+                modified.write('Author-Workplace-Name: Williams College' + '\n')
+                modified.write('Author-Workplace-Homepage: https://econ.williams.edu/profile/' + Author_Homepage[lastName[i]] + '\n')
+            # Non-Williams Faculty
+            else:
+                modified.write('Author-Workplace-Name: ' + '\n')
+                modified.write('Author-Workplace-Homepage: ' + '\n')
+
+        modified.write("Title: " + title + '\n')
+        modified.write("Abstract: " + '\n')
+        modified.write("Classification-JEL: " + clasJEL + '\n')
+        modified.write("Keywords: " + keywords + '\n')
+        modified.write('Length: ' + length + ' pages' + '\n')
+        modified.write("Number: " + curYear + "-" + number + '\n')
+        modified.write("Note: " + '\n')
+        modified.write('Creation-date: ' + created + '\n')
+        modified.write('Revision-date:' + '\n' + 'Price: Free' + '\n' + 'Publication-Status:' + '\n')
+        modified.write('File-URL: http://web.williams.edu/Economics/wp/'+ lastNames + urlTitle + '.pdf\n')
+
+        modified.write('File-Format: Application/PDF' + '\n')
+        modified.write('File-Function: Full text' + '\n')
+        modified.write('Handle: RePEc:wil:wileco:' + curYear + '-' + number + '\n\n')
+
+        modified.write(data)
+
+    modified.close()
+
+###############################################################
+def main():
+    simpleAddEntry()
+if __name__ == '__main__':
+    main()
